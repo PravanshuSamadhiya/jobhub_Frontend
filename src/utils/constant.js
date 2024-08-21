5472246b4232706954a05020d3a52d335a8c76e9
Abhi samadhiya <abhisamadhiya@Abhis-MacBook-Air.local>
2024-08-21 15:59:47 +0530

    your updated backend code

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'UweOVqrUokCH0ADZrbpY73vM5M4';
+const API_BASE_URL = 'https://jobhub-backend-1r8j.onrender.com';
 export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/api/v1/jobs`;
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/api/v1/application`;
